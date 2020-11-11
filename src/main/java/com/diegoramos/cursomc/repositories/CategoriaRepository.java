36227d785c7e8afa453943ba854bff5de0250987
Diego Ramos <diegopsr2010@gmail.com>
2020-11-11 19:15:29 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/diegoramos/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.diegoramos.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.diegoramos.cursomc.domain.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+	
+	
+}
